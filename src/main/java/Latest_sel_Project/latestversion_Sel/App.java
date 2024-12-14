42cfef80e4cac0b2f135ebfcc00e48e8fdce58a3
TEJA <maskaniraviteja1997@gmail.com>
2024-12-14 11:59:05 +0530

    first commit

---
+++ src/main/java/Latest_sel_Project/latestversion_Sel/App.java
@@ -0,0 +1,13 @@
+package Latest_sel_Project.latestversion_Sel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
